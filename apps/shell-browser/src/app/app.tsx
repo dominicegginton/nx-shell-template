b3af96f13f800d7aead16d3681cb870e42b56c09
Dominic Egginton <dominic.egginton@gmail.com>
2023-10-12 16:50:47 +0100

    init

---
+++ apps/shell-browser/src/app/app.tsx
@@ -0,0 +1,10 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import styles from './app.module.css';
+
+import { Layout } from 'layout';
+
+export function App() {
+  return (<Layout />);
+}
+
+export default App;
